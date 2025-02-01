592a769562e0551d34d854e2977b5cc41ebeef87
Keith McDade <kp.mac09@gmail.com>
2025-01-31 23:16:14 -0500

    added some tests

--- add.h
+++ add.h
@@ -1,19 +1,21 @@
-#ifndef ADD_H
-#define ADD_H
+#include <string>
+#pragma once
+// #ifndef ADD_H
+// #define ADD_H
 
 // validates string of numbers for evaluation
-bool valid(string s);
+bool valid(std::string s);
 
 // wrapper that redirects input based on negative or positive quality
-string add(string s1, string s2);
+std::string add(std::string s1, std::string s2);
 
 // wrapper that does some housekeeping for 2 negative numbers
-string add_negative(string s1, string s2);
+std::string add_negative(std::string s1, std::string s2);
 
 // wrapper that does housekeeping for 1 negative and 1 positive number
-string subtract(string s1, string s2);
+std::string subtract(std::string s1, std::string s2);
 
 // adds or subtracts number
-string calculate(string s1, string s2, bool add);
+std::string calculate(std::string s1, std::string s2, bool add);
 
-#endif
+// #endif
