a7e5b80c2bf25c0afc9faffe2182baf3ae897086
Keith McDade <kp.mac09@gmail.com>
2025-02-10 15:07:20 -0500

    refactored to use 2d array instead of struct

--- score_race.cpp
+++ score_race.cpp
@@ -2,94 +2,75 @@
 #include <iomanip>
 using namespace std;
 
-const int MAX_TEAMS = 26;
-
-struct Team {
-    inline static int team_count;
-    inline static int team_size;
-    int members = 0;
-    double score = 0;
-    char name;
-};
-
-void create_teams(string input, Team teams[]);
-bool check_size(Team (&teams)[MAX_TEAMS]);
-void print_winner(Team (&teams)[MAX_TEAMS]);
+bool check_size(int teams[][2], int n, int team_size);
 
 int main() {
-    
-    while (true) {
 
-        Team teams[MAX_TEAMS];
-        Team::team_count = 0;
-        Team::team_size = 0;
-        string input;
+    const int WIDTH = 6, MAX_TEAMS = 26;
+    
+    while(true) {
+        int teams[26][2] = {0}, team_count = 0, team_size = 0;
+        double best_score = 0;
+        string input, winners;
 
         cout << "Enter a string of only uppercase letters or 'done' to exit: ";
         if (cin >> input && input.compare("done") == 0) return 0;
-    
+        // validate input for only char A-Z
         if (input.find_first_not_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ") != string::npos) {
             cout << endl << "Invalid character sequence, please try again." << endl;
             continue;
         }
+    
+        // iterate through input, teams[i][0] == no. of runners, teams[i][1] == score
+        for (int i = 0; i < input.length(); i++) {
+            
+            char t = input[i] - 'A';
+            if (teams[t][0] == 0) team_count++;
 
-        create_teams(input, teams);
-        if (!check_size(teams)) continue;
+            teams[t][0]++;
+            teams[t][1] += i + 1;
 
-        print_winner(teams);
+            if (teams[t][0] > team_size) team_size++;
+        }
+        
+        // check if each team is the same size
+        if (!check_size(teams, MAX_TEAMS, team_size)) continue;
+        cout << endl << "There are " << team_count << " teams." << endl << endl <<
+            "Each team has " << team_size << " runners." << endl << endl << setw(WIDTH) << 
+            left << "Team" << "Score" << endl;       
+
+        // iterate through teams add team to winners if score == best_score, clear other winners if better
+        for (int i = 0; i < MAX_TEAMS; i++) {
+
+            if (teams[i][0] == 0) continue;
+        
+            double score = (double)teams[i][1] / teams[i][0];
+            if (best_score == 0 || score < best_score) {
+                best_score = score;
+                winners = "";
+            }
+            if (score <= best_score) winners += (char)i + 'A';
+
+            cout << setw(WIDTH) << left << setprecision(3) << 
+                (char)(i + 'A') << score << endl;
+        }
+
+        // print results
+        cout << endl;
+        for (int i = 0; i < winners.length(); i++) 
+            cout << "The winning team is " << winners[i] << " with a score of " << best_score << endl;
+        cout << endl;
     }
     return 0;
 }
 
-void create_teams(string input, Team teams[]) {
-
-    for (int i = 0; i < input.length(); i++) {
-
-        Team *team = &teams[input[i] - 'A'];
-        if (team->members == 0) Team::team_count++;
-
-        team->name = input[i];
-        team->members += 1;
-        team->score += i + 1;
-
-        if (team->members > Team::team_size) Team::team_size = team->members;
-    }            
-}
-
-bool check_size(Team (&teams)[MAX_TEAMS]) {
-    for (Team team : teams) {
-        if (team.members == 0) continue;
-        if (team.members != Team::team_size) {
+bool check_size(int teams[][2], int n, int team_size) {
+    for (int i = 0; i < n; i++) {
+        if (teams[i][0] == 0) continue;
+        if (teams[i][0] != team_size) {
             cout << endl << "There must be an equal number of members for each team, please try again" << endl;
             return false;
         }
     }
     return true;
-}
-
-void print_winner(Team (&teams)[MAX_TEAMS]) {
-
-    const int WIDTH = 6;
-    double high_score = 0;
-    Team winner;
-
-    cout << endl << "There are " << Team::team_count << " teams." << endl << endl <<
-            "Each team has " << Team::team_size << " runners." << endl << endl <<
-            setw(WIDTH) << left <<
-            "Team" << "Score" << endl;
-
-    for (Team team : teams) {
-        
-        if (team.members == 0) continue;
-        team.score = team.score / team.members;
-
-        if (team.score < high_score || high_score == 0) {
-            high_score = team.score;
-            winner = team;
-        }
-
-        cout << setw(WIDTH) << left << setprecision(3) << 
-                team.name << team.score << endl;
-    }
-    cout << endl << "The winning team is " << winner.name << " with a score of " << winner.score << endl << endl;
 }
