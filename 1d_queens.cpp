62344f522aabe74a3f1f640591665e0dde6e4032
Keith McDade <kp.mac09@gmail.com>
2025-02-21 15:47:33 -0500

    added validty test for N queens problem

--- 1d_queens.cpp
+++ 1d_queens.cpp
@@ -3,40 +3,48 @@ using namespace std;
 
 int main() {
 
-    const int Q = 8;
-    int q[Q], c = 0, solutions = 0;
+    // N = size of board
+    const int N = 8;
+    int q[N], c = 0, solutions = 0;
     q[c] = 0;
 
 nc:
+    // iterate c, if c == size of board print solution
     c++;
-    if (c == Q) goto print;
+    if (c == N) goto print;
+    // enables use of q[c]++ at label nr without skipping 0
     q[c] = -1;
 
 nr:
     q[c]++;
-    if (q[c] == Q) goto backtrack;
+    // if row == size of board there is no valid spot in that col, go back one col
+    if (q[c] == N) goto backtrack;
     // row test + diagonal test
-    
+    for (int i = 0; i < c; i++) 
+        // row test | down and left test | up and left test
+        // down and left: row # col #; up and left: row # - col #
+        // go back to new row if test fails
+        if (q[i] == q[c] || q[i] + i == q[c] + c || q[i] - i == q[c] - c) goto nr;
 
-    q[c] = 1;
     goto nc;
 
 backtrack:
     c--;
+    // if c < 0 there are no more valid solutions
     if (c == -1) return 0;
-    // reset previous queen placement
-    q[c] = 0;
-    while (q[c] != 1) q[c]++;
-    q[c] = 0;
-    
+
     goto nr;
 
 print:
     solutions++;
     cout << endl << "solution #" << solutions << ":" << endl;
     cout << "---------------" << endl;
-    for (int i = 0; i < Q; i++) {
-        // print
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (q[i] == j) cout << 1 << " ";
+            else cout << 0 << " ";
+        }
+        cout << endl;
     }
     
     goto backtrack;
