9df5ded7e80e1bf937eec30273dce469cfe583a1
Keith McDade <kp.mac09@gmail.com>
2025-03-05 22:31:49 -0500

    8 numbers in a cross problem. Uses same main() as 8 queens (ok_queens.cpp)

---
+++ cross.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+bool ok(int q[], int c);
+void print(int q[], int n, int solutions);
+
+int main() {
+
+    // n = size of board
+    const int n = 8;
+    int q[n], c = 0, solutions = 0;
+    q[c] = 1;
+
+    while (c >= 0) {
+        
+        c++;
+        // if c == n print solution
+        if (c == n) {
+            print(q, n, ++solutions); // increment solutions before print is called
+            c--;
+        } else q[c] = 0;
+        
+        while (q[c] < n + 1) {
+            q[c]++;
+            // if q[c] == n there is no valid number for that box, backtrack
+            if (q[c] == n + 1) {
+                c--;
+                continue;
+            }
+            if (ok(q, c)) break;
+        }
+    }
+    return 0;
+}
+
+bool ok(int q[], int c) {
+
+    /* 
+    adjacency table:
+        # 0 1 #
+        2 3 4 5
+        # 6 7 #
+    */  
+    int a[8][5] = {{-1}, {0, -1}, {0, -1}, {0, 1, 2, -1}, {0, 1, 3, -1}, {1, 4, -1}, {2, 3, 4, -1}, {3, 4, 5, 6, -1}};
+    
+    // "row" test
+    for (int i = 0; i < c; i++) {
+        if (q[c] == q[i]) return false;
+    }
+    
+    int i = 0;
+    // adjacency test - a[c][i] == adjacent box to check
+    while (a[c][i] != -1) {
+        if (abs(q[c] - q[a[c][i]]) == 1) 
+            return false;
+        i++;
+    }
+
+    return true;
+}
+
+void print(int q[], int n, int solutions) {
+
+    int x = 0;
+
+    cout << endl << "solution #" << solutions << ":" << endl;
+    cout << "---------------" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (i % 2 == 0 && (j == 0 || j == 3)) cout << "# ";
+            else {
+                cout << q[x] << " ";
+                x++;
+            }
+        }
+        cout << endl;
+    }
+}
